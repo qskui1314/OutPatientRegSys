1e6688e8c81ca212f7dd086a89a5438564440f65
J.L <lijing79@outlook.com>
2017-10-20 21:08:06 +0800

    updateSrc

---
+++ Src/OutpatientRegistrationSystem/Frm_Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OutpatientRegistrationSystem
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void 退出系统ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("您确定要退出系统吗?", "退出确认", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dr == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+    }
+}
