8c8d3163529ded57ba2c140f38fedb74fdfcf634
qskui1314 <1353842241@qq.com>
2017-11-05 23:24:36 +0800

    Nightly update 2017.11.5
    Added installation files.

--- Src/OutpatientRegistrationSystem/Frm_huanhao.Designer.cs
+++ Src/OutpatientRegistrationSystem/Frm_huanhao.Designer.cs
@@ -28,20 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lb_kahao = new System.Windows.Forms.Label();
+            this.tb_kahao = new System.Windows.Forms.TextBox();
+            this.lb_binglihao = new System.Windows.Forms.Label();
+            this.tb_binglihao = new System.Windows.Forms.TextBox();
+            this.bt_chaxun = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lb_kahao
+            // 
+            this.lb_kahao.AutoSize = true;
+            this.lb_kahao.Location = new System.Drawing.Point(12, 9);
+            this.lb_kahao.Name = "lb_kahao";
+            this.lb_kahao.Size = new System.Drawing.Size(29, 12);
+            this.lb_kahao.TabIndex = 0;
+            this.lb_kahao.Text = "卡号";
+            // 
+            // tb_kahao
+            // 
+            this.tb_kahao.Location = new System.Drawing.Point(84, 7);
+            this.tb_kahao.Name = "tb_kahao";
+            this.tb_kahao.Size = new System.Drawing.Size(206, 21);
+            this.tb_kahao.TabIndex = 1;
+            // 
+            // lb_binglihao
+            // 
+            this.lb_binglihao.AutoSize = true;
+            this.lb_binglihao.Location = new System.Drawing.Point(423, 9);
+            this.lb_binglihao.Name = "lb_binglihao";
+            this.lb_binglihao.Size = new System.Drawing.Size(41, 12);
+            this.lb_binglihao.TabIndex = 2;
+            this.lb_binglihao.Text = "病历号";
+            // 
+            // tb_binglihao
+            // 
+            this.tb_binglihao.Location = new System.Drawing.Point(483, 7);
+            this.tb_binglihao.Name = "tb_binglihao";
+            this.tb_binglihao.Size = new System.Drawing.Size(204, 21);
+            this.tb_binglihao.TabIndex = 3;
+            // 
+            // bt_chaxun
+            // 
+            this.bt_chaxun.Location = new System.Drawing.Point(325, 4);
+            this.bt_chaxun.Name = "bt_chaxun";
+            this.bt_chaxun.Size = new System.Drawing.Size(75, 23);
+            this.bt_chaxun.TabIndex = 4;
+            this.bt_chaxun.Text = "查询";
+            this.bt_chaxun.UseVisualStyleBackColor = true;
             // 
             // Frm_huanhao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(699, 461);
+            this.Controls.Add(this.bt_chaxun);
+            this.Controls.Add(this.tb_binglihao);
+            this.Controls.Add(this.lb_binglihao);
+            this.Controls.Add(this.tb_kahao);
+            this.Controls.Add(this.lb_kahao);
             this.Name = "Frm_huanhao";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "换号";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb_kahao;
+        private System.Windows.Forms.TextBox tb_kahao;
+        private System.Windows.Forms.Label lb_binglihao;
+        private System.Windows.Forms.TextBox tb_binglihao;
+        private System.Windows.Forms.Button bt_chaxun;
     }
 }
